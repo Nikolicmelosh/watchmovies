b8c528f37f36e40354655a66763fdee363e6f628
Nikolicmelosh <141958973+Nikolicmelosh@users.noreply.github.com>
2025-08-04 03:32:34 +0000

    lookmovie_scraper.py

--- lookmovie_scraper.py
+++ lookmovie_scraper.py
@@ -1,8 +1,13 @@
 from flask import Flask, request, jsonify
 import requests
 from bs4 import BeautifulSoup
+import os
 
 app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "LookMovie scraper is running."
 
 @app.route("/search")
 def search():
@@ -41,4 +46,5 @@ def search():
     return jsonify(results)
 
 if __name__ == "__main__":
-    app.run(port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
